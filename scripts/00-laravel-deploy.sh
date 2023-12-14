d7a29169e5d38e190509983378dcbef7d5576964
highb33kay <hiibeekayvibe@gmail.com>
2023-12-14 11:33:32 +0100

    hotfix

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 echo "Running composer"
+cp /etc/secrets/.env .env
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
+
+echo "Clearing caches..."
+php artisan optimize:clear
 
 echo "Caching config..."
 php artisan config:cache
@@ -11,3 +15,5 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "done deploying"
